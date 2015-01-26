ba836a4c64a3494cd2ad0a38fc6b3a83ac2e9748
Dave Bennett <dmbennett@anderson.edu>
2015-01-26 16:52:22 -0500

    mvp complete

--- quizzler.rb
+++ quizzler.rb
@@ -1,14 +1,14 @@
 def is_true_or_false?(answer)
-  if answer == "true" || answer == "t"
+  if answer == "true" || answer == "t" || answer == true
     return true
-  elsif answer == "false" || answer == "f"
+  elsif answer == "false" || answer == "f" || answer == false
     return false
   else
+    "puts nil"
     return nil
   end
 end
 
-def grade_the_quiz ()
 
 teacher_q_and_a = {
   "True or False: '<!--...-->' defines a comment in html?" => true,
@@ -25,14 +25,19 @@ teacher_q_and_a = {
 
 question_number = 0
 student_quiz_answers = {}
-teacher_q_and_a.keys.each do |question, correct_answer|
+number_of_correct_answers = 0
+teacher_q_and_a.each do |question, correct_answer|
   question_number += 1
   puts "#{question_number}. #{question}"
-  answer = gets.chomp.downcase
-  while is_true_or_false?(answer) != true && is_true_or_false?(answer) != false
+  answer = is_true_or_false?(gets.chomp.downcase)
+  while is_true_or_false?(answer) == nil
     puts "Please enter only 'true' or 'false': "
     answer = gets.chomp.downcase
   end
   student_quiz_answers[question] = answer
+  if correct_answer == student_quiz_answers[question]
+     number_of_correct_answers +=1
+  end
 end
-
+grade = (number_of_correct_answers.to_f / teacher_q_and_a.length)
+puts "You scored a #{grade*100}%!"
